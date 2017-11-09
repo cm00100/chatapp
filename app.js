be1cffc4eaa0044f3e9c7a91be5216103be4fc0e
Cynthia Martinez <cyn.mart@outlook.com>
2017-11-09 08:02:29 +0000

    add

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+var port = process.env.PORT || 8080;
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
